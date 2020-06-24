cc2289d4b0d68639534735860b46d4420184a906
Ludwig Stumpp <42147848+LudwigStumpp@users.noreply.github.com>
2020-06-25 01:51:43 +0200

    first working version

---
+++ index.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    iconClass: 'fa-github',
+    iconColor: '#000000',
+    backgroundColor: '#f59342',
+    fontSize: 150,
+    borderRadius: 50,
+    padding: 20,
+  },
+  methods: {
+    printToCanvas: function (event) {
+      html2canvas(document.querySelector("#socialIcon")).then(canvas => {
+        const image = canvas.toDataURL("image/png");
+        let w = window.open('about:blank', 'image from canvas');
+        w.location.href = image;
+      });
+    }
+  }
+})
